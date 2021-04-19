05d37ea7fee0758d0cea90abdef29e66fc57a676
alexey <alexey@alexey.komitex.ru>
2021-04-19 16:33:09 +0300

    default return args test

--- 1/default_args.cc
+++ 1/default_args.cc
@@ -2,23 +2,27 @@
 #include <string>
 #include <type_traits>
 
-// user std::decay_t to ensure to reference can be returned
-// c++14
-// requires to call default constructors
-template<typename T1, typename T2, typename RT = std::decay_t<decltype(true ? T1() : T2())>>
+#if 0 
+// ensure that no reference can be returned
+// this implementation requires default constructors for the passed types
+template<typename T1, typename T2,
+	 typename RT = std::decay_t<decltype(true ? T1() : T2())>>
 RT max(T1 a, T2 b)
 {
 	return b < a ? a : b;
 }
+#endif
 
-template<typename T1, typename T2, typename RT = std::common_type_t<T1, T2>>
-RT _max(T1 a, T2 b)
+// std::common_type_t decays so that the return value cannot become a reference
+template<typename T1, typename T2,
+	 typename RT = std::common_type_t<T1, T2>>
+RT max(T1 a, T2 b)
 {
 	return b < a ? a : b;
 }
 
 int main(int argc, char *argv[])
 {
-
+	::max(1, 2);
 	return 0;
 }
