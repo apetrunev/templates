bc0c599c1f397e09f3c593ff82037a4bb886c10a
alexey <alexey@alexey.komitex.ru>
2021-04-20 16:31:17 +0300

    updated stack.h

--- 2/stack.h
+++ 2/stack.h
@@ -1,46 +1,32 @@
-#ifndef STACK_H_
-#define STACK_H_
+#pragma once
 
-#include <cassert>
-#include <iostream>
 #include <vector>
+#include <cassert>
 
-template <typename T>
+template<typename T>
 class Stack {
 private:
 	std::vector<T> elems;
 public:
-	void push(T const& elem);
-	void pop();
-	T const& top() const;
-	bool empty() const {
-		return elems.empty();
-	}
-	// print stack content
-	void printOn(std::ostream &strm) const {
-		for (T const &elem : elems) {
-			strm << elem << ' ';
-		}
-	}
-	// templated entity
-	// instantiated with the class template if needed
-	friend std::ostream& operator<< (std::ostream& strm, Stack<T> const& s) {
-		s.printOn(strm);
-		return strm;
+	void push(T const& elem); // push element
+	void pop();		  // pop element
+	T const& top() const;	  // return top element
+	bool empty() const {	  // return whether the stack is empty
+		return elems.empty();	
 	}
 };
 
 template<typename T>
-void Stack<T>::push(T const & elem)
+void Stack<T>::push(T const& elem)
 {
-	elems.push_back(elem); // append copy of passed elem
+	elems.push_back(elem);	// append copy of passed elem
 }
 
 template<typename T>
 void Stack<T>::pop()
 {
 	assert(!elems.empty());
-	elems.pop_back(); // remove the last element
+	elems.pop_back();	// remove last element
 }
 
 template<typename T>
@@ -49,5 +35,3 @@ T const& Stack<T>::top() const
 	assert(!elems.empty());
 	return elems.back(); // return copy of last element
 }
-
-#endif // STACK_H_
