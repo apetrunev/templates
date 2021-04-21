0a2d316e6d5c5c51bc6de332e949b2ba02a235dc
alexey <alexey@alexey.komitex.ru>
2021-04-21 14:03:17 +0300

    fixed type in Makfile and stack.cc

--- 2/stack.cc
+++ 2/stack.cc
@@ -1,9 +1,18 @@
 #include "Stack.h"
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
-	Stack<int> inStack;
+	Stack<int> intStack;
+	Stack<std::string> stringStack;
+
+	intStack.push(7);
+	std::cout << intStack.top() << std::endl;
+
+	stringStack.push("hello");
+	std::cout << stringStack.top() << std::endl;
+	stringStack.pop();	
 	return 0;
 }
